31ad1d44a171f54cccdc691112f180914fb5e46e
Yuriy Nemtsov <nemtsov@gmail.com>
2018-08-04 02:15:24 -0400

    clarify

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) =>  {
-  res.end('ok');
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+      <headD>
+        <style>
+          * {
+            font-family: 'Helvetica Neue'
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Crafter carefully for you</h1>
+      </body>
+    </html>
+  `);
 });
 
 server.listen(4000);
